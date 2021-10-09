23d99b68e81c85678d93b1a465df92e788ce5572
Lmsilvano <leandro.m.silvano@gmail.com>
2021-10-08 21:44:30 -0300

    .
    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,88 @@
 const sContent = document.querySelectorAll(".s_content")
 const tl = new TimelineMax()
 
-function animation(arg) {
-    tl.fromTo(arg, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut });
+function animeExpand(arg) {
+    tl.fromTo(arg, 1, { height: "0" }, { height: "450px", ease: Power2.easeInOut });
 
 }
 
 function animationO(elem, arg) {
     if (arg === 'in') {
-        elem.classList.add('faded-in');
+        elem.style = 'opacity:1'
         requestAnimationFrame(() => {
-            elem.classList.remove('faded-in');
+            elem.style = 'opacity: 0';
         });
+
+        return
+        //elem.classList.add('faded-in');
+        //requestAnimationFrame(() => {
+        //   elem.classList.remove('faded-in');
+        // });
+    } else if (arg === 'out') {
+
+        elem.style = 'opacity:0'
+        requestAnimationFrame(() => {
+            elem.style = 'opacity: 1';
+        });
+        return
+        // elem.classList.add('faded-out');
+        // requestAnimationFrame(() => {
+        //     elem.classList.remove('faded-out');
+        // });
     } else {
         elem.classList.add('faded-out');
         requestAnimationFrame(() => {
             elem.classList.remove('faded-out');
         });
+
     }
 }
-//
 
+function teste(algo) {
+    algo.style = "display:flex; flex-direction: column"
+}
+//w 319 h 390
+async function fullanime(elem, arg, elem2, arg2, arg3) {
+    Promise.resolve(animationO(elem, arg))
+        .then(() => { animation(arg3) })
+        .then(() => teste(elem2))
+        .then(() => animationO(elem2, arg2))
+
+}
 
 document.addEventListener("click", function (e) {
     e.preventDefault();
     const el = e.target;
     if (el.classList.contains('reveal')) {
         let divreveal = el.nextElementSibling;
-        animationO(divreveal, 'oi')
-        divreveal.style.display = "inline-block";
-        el.style.display = "none"
-        animationO(el, 'in')
+        let parent = el.parentElement;
+        animation(parent)
+        divreveal.style = "display:flex; flex-direction: column"
+        animationO(divreveal, 'ooi')
+        //fullanime(el, 'in', divreveal, 'n', parent)
 
 
-        divreveal.addEventListener("mouseleave", function (e2) {
-            e2.preventDefault();
-            divreveal.style.display = "none"
-            animationO(el, 'oi')
-            el.style.display = 'block'
 
-        })
+        if (el.style.opacity === '0') {
+            animationO(el, 'out')
+            return
+        } else {
+            animationO(el, 'in')
+            // setTimeout(() => {
+            //     el.remove();
+            // }, 1500)
+
+        }
+
+
+        // divreveal.addEventListener("mouseleave", function (e2) {
+        //     e2.preventDefault();
+        //     // divreveal.style.display = "none"
+        //     animationO(el, 'oi')
+        //     console.log(el)
+        //     el.style.display = 'block'
+
+        // })
 
     };
 });
