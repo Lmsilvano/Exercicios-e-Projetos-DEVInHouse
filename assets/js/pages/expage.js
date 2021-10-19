6efd6c4567f37a097462b4322821377ecaa9e79c
Lmsilvano <leandro.m.silvano@gmail.com>
2021-10-19 00:40:57 -0300

    Correções
    ancoras e margin

--- assets/js/pages/expage.js
+++ assets/js/pages/expage.js
@@ -1,12 +1,12 @@
 const exData = [{
     tittle: 'Cadastro de livros', description: `exercícios de construção de sistema de cadastro de livros proposto 
-em aula do primeiro módulo do curso. Dados são salvos no Local Storage do Browser.`, url: "./Ex/cadastrobiblio/index.html", git: ''
+em aula do primeiro módulo do curso. Dados são salvos no Local Storage do Browser.`, url: "./Ex/cadastrobiblio/index.html", git: 'https://github.com/Lmsilvano/Exercicios-e-Projetos-DEVInHouse/tree/main/Ex/cadastrobiblio'
 },
 {
-    tittle: 'Cadastro de alunos', description: `exercícios de construção de sistema de cadastro de multiplos alunos, separados por ";", com retorno de situação.`, url: "./Ex/cadastroalunos/index.html", git: ''
+    tittle: 'Cadastro de alunos', description: `exercícios de construção de sistema de cadastro de multiplos alunos, separados por ";", com retorno de situação.`, url: "./Ex/cadastroalunos/index.html", git: 'https://github.com/Lmsilvano/Exercicios-e-Projetos-DEVInHouse/tree/main/Ex/cadastroalunos'
 },
 {
-    tittle: 'Cadastro de produtos', description: `exercícios de construção de sistema de cadastro de produtos, com pagina de relatório para impressão.`, url: "./Ex/cadastroprodut/form.html", git: ''
+    tittle: 'Cadastro de produtos', description: `exercícios de construção de sistema de cadastro de produtos, com pagina de relatório para impressão.`, url: "./Ex/cadastroprodut/form.html", git: 'https://github.com/Lmsilvano/Exercicios-e-Projetos-DEVInHouse/tree/main/Ex/cadastroprodut'
 }]
 
 export default exData
