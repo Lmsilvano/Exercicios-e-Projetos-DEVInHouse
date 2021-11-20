f2ccca9dac6e85b6967949247e0bc142c2c521c1
Lmsilvano <leandro.m.silvano@gmail.com>
2021-11-20 14:06:31 -0300

    inclusao
    .

--- assets/js/pages/mainpage.js
+++ assets/js/pages/mainpage.js
@@ -4,7 +4,7 @@ uma lista de tarefa de layout simples e intuitivo, onde o usuário pode adiciona
 marca-las e depois deletá-las. Os dados correspondentes as atividades e as interações que o usuário realizou comas tarefas
 devem ficar salvos no Local Storage do browser, 
 para serem reimpressos na tela ao se fechar o browser e depois retornar a aplicação.`, url: "https://lmsilvano.github.io/Atividade_To-do-DEVinHouse/", git: 'https://github.com/Lmsilvano/Atividade_To-do-DEVinHouse'
-}]
+}, { tittle: 'DEVInMMO [REACT]', description: 'O Projeto consiste em uma aplicação simples, construída em React e consumindo a api MMO Games API através do serviço Rapid API, com uma página listando as notícias, outra listando os games, e uma terceira com detalhes sobre o respectivo game selecionado na página anterior de listagem dos games. ', url: 'https://sleepy-sammet-4b6cba.netlify.app/', git: 'https://github.com/Lmsilvano/DevInMMO-Projeto2' }]
 
 
 export default projectData
